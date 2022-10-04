072e188396ca69a6e22e7cc65b6b2a325f3c6f62
Jannik Baumgarten <Jannik.Baumgarten@vwfsdev.com>
2022-10-04 14:41:16 +0200

    Category Page 1

--- CookyBook/DataAccess/Repositories/Impl/CookBookRepository.cs
+++ CookyBook/DataAccess/Repositories/Impl/CookBookRepository.cs
@@ -111,5 +111,40 @@ namespace DataAccess.Repositories.Impl
                 con.Close();
             }
         }
+
+        public Category[] GetCategory()
+        {
+            List<Category> CategoryList = new List<Category>();
+
+            string conString = "Data Source=(localdb)\\MsSqlLocalDB;Initial Catalog=CookyBook_DB;Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(conString);
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Category";
+
+            try
+            {
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Category category = new Category();
+                    category.Id = reader.GetInt32(0);
+                    category.Title = reader.GetString(reader.GetOrdinal("Title"));
+                    category.Description = reader.GetString(reader.GetOrdinal("Description"));
+                    CategoryList.Add(category);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return CategoryList.ToArray();
+        }
     }
 }
