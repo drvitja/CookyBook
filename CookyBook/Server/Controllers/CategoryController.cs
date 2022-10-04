9bacf6633171b3789ccf873107937cd7d519a09d
Jannik Baumgarten <Jannik.Baumgarten@vwfsdev.com>
2022-10-04 14:22:57 +0200

    Category Page

---
+++ CookyBook/Server/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CookyBook.Shared.DataTransferObjects;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookyBook.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICookBookRepository repos;
+        private readonly IMapper mapper;
+
+        public CategoryController(ICookBookRepository repos, IMapper mapper)
+        {
+            this.repos = repos;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<CategoryDto> Get()
+        {
+            List<CategoryDto> CategoryDtos = new();
+            mapper.Map(repos.GetCategory(), CategoryDtos);
+            return CategoryDtos.ToArray();
+        }
+    }
+}
---
+++ CookyBook/Shared/DataTransferObjects/CategoryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CookyBook.Shared.DataTransferObjects
+{
+    public class CategoryDto : DtoBase
+    {
+        public bool Selected { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+}
