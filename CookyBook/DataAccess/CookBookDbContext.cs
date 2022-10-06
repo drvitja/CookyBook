6ba918c377b5182a5ff480716677390637729eae
Sven Grigowski <sven.grigowski1@vwfs.com>
2022-10-06 12:32:52 +0200

    Initial Db Context

---
+++ CookyBook/DataAccess/CookBookDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class CookBookDbContext : DbContext
+    {
+        public CookBookDbContext(DbContextOptions<CookBookDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        DbSet<Category>? Categories { get;set;}
+
+        DbSet<Ingredient>? Ingredients { get;set;}
+
+        DbSet<Recipe>? Recipes { get; set; }
+
+    }
+}
